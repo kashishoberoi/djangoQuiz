1ffcaafe5a95393d8dd7b23e592da659f7d47fd8
KompriseOberoi <kashish.oberoi@komprise.com>
2021-02-08 00:43:21 +0530

    model fixes

--- portal/models.py
+++ portal/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from quiz.models import *
 from question.models import *
+import datetime
 # Create your models here.
 
 class userQuiz(models.Model):
     quiz_user = models.ManyToManyField(User)
     quiz_object = models.ManyToManyField(quizC)
     quiz_marks_obtained = models.IntegerField(default=0)
+    quiz_user_created_at = models.DateField(default=datetime.now,blank=True)
 
 class userQuestionMarks(models.Model):
     quiz_user_object = models.ManyToManyField(userQuiz)
--- quiz/models.py
+++ quiz/models.py
@@ -16,7 +16,7 @@ class quizC(models.Model):
     quiz_id = models.AutoField(primary_key=True)
     quiz_title = models.CharField(max_length=50)
     quiz_desc = models.CharField(max_length=200)
-    quiz_total_marks = models.IntegerField()
+    quiz_total_marks = models.IntegerField(default=0)
     subjects_objects = models.ManyToManyField(quizSubject)
     maxTime = models.DurationField(default=datetime.timedelta(hours=1))
     
